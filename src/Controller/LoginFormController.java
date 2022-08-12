3b0eaa330e08197770696fd17a1facdf8382a340
DilanTharaka <tharakad049@gmail.com>
2022-08-13 00:03:42 +0530

    update login form

--- src/Controller/LoginFormController.java
+++ src/Controller/LoginFormController.java
@@ -1,4 +1,25 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+
+    public JFXTextField txtUsername;
+    public JFXPasswordField txtPassword;
+    public JFXButton btnForgotPassword;
+    public JFXButton btnLogin;
+    public JFXButton btnSignUp;
+
+    public void forgotPasswordOnAction(ActionEvent actionEvent) {
+        
+    }
+
+    public void signupOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/Controller/RegisterFormController.java
+++ src/Controller/RegisterFormController.java
@@ -1,4 +1,22 @@
 package Controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class RegisterFormController {
+    public AnchorPane registerContext;
+    public JFXTextField txtUsername;
+    public JFXPasswordField txtPassword;
+    public JFXButton btnSignUp;
+    public JFXTextField txtName;
+    public JFXButton btnLogin;
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signUpOnAction(ActionEvent actionEvent) {
+    }
 }
